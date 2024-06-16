d41969886aa8dca5be436eb9e3859bc6feb4b964
Dan <daschwanden@google.com>
2024-06-16 08:06:39 +0000

    Initial commit

---
+++ upload-pipeline.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Google.
+# This software is provided as-is, without warranty or representation for any use or purpose.
+# Your use of it is subject to your agreement with Google.
+import os
+from kfp.registry import RegistryClient
+
+region = os.environ.get("_REGION", "")
+repo_name = os.environ.get("REPO_NAME", "")
+project_id = os.environ.get("PROJECT_ID", "")
+host = "https://"+region+"-kfp.pkg.dev/"+project_id+"/"+repo_name
+client = RegistryClient(host=host)
+
+templateName, versionName = client.upload_pipeline(
+  file_name="hello_world_pipeline.yaml",
+  tags=[os.environ.get("SHORT_SHA", "latest"), "prod"],
+  extra_headers={"description":"This is an example pipeline template."})
+
