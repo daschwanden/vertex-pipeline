42ae43a8e74a7ab8c02bdb203476355194f9be67
Dan <daschwanden@google.com>
2024-06-17 08:47:42 +0000

    Add more tags

--- upload-pipeline.py
+++ upload-pipeline.py
@@ -2,6 +2,7 @@
 # This software is provided as-is, without warranty or representation for any use or purpose.
 # Your use of it is subject to your agreement with Google.
 import os
+import datetime
 from kfp.registry import RegistryClient
 
 region = os.environ.get("_REGION", "")
@@ -12,6 +13,6 @@ client = RegistryClient(host=host)
 
 templateName, versionName = client.upload_pipeline(
   file_name="hello_world_pipeline.yaml",
-  tags=[os.environ.get("SHORT_SHA", "latest")],
+  tags=[os.environ.get("SHORT_SHA", "latest"), os.environ.get("BRANCH_NAME", ""), "feat:"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")],
   extra_headers={"description":"This is an example pipeline template."})
 
