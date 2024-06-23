12620e5ecdb2dfcda668d559dada7ffa82604c6e
Dan <daschwanden@google.com>
2024-06-23 14:43:27 +0000

    fix feature branch

--- scripts/upload-pipeline.py
+++ scripts/upload-pipeline.py
@@ -5,19 +5,32 @@ import os
 import datetime
 from kfp.registry import RegistryClient
 from sha256sum import sha256sum
+import hashlib
+
+def hash_signature(signature):
+ # Encode the signature as bytes
+ signatured_bytes = signature.encode('utf-8')
+ # Use SHA-256 hash function to create a hash object
+ hash_object = hashlib.sha256(signature_bytes)
+ # Get the hexadecimal representation of the hash
+ signature_hash = hash_object.hexdigest()
+ return signature_hash
 
 region = os.environ.get("_REGION", "")
 repo_name = os.environ.get("REPO_NAME", "")
 project_id = os.environ.get("PROJECT_ID", "")
+package = os.environ.get("PACKAGE", "")
+signature = os.environ.get("SIGNATURE", "")
+signature_hash = hash_signature(signature)
 host = "https://"+region+"-kfp.pkg.dev/"+project_id+"/"+repo_name
 client = RegistryClient(host=host)
 
-source_name = "hello-world-pipeline.py"
-pipe_name = "hello_world_pipeline.yaml"
+source_name = "pipeline.py"
+compiled_name = package+".yaml"
 sha256 = sha256sum(source_name)
 
 templateName, versionName = client.upload_pipeline(
-  file_name=pipe_name,
-  tags=[os.environ.get("BRANCH_NAME").replace("/", "-")+"-"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")+"-"+os.environ.get("SHORT_SHA", "latest"), "source:"+sha256],
+  file_name=compiled_name,
+  tags=[os.environ.get("BRANCH_NAME").replace("/", "-")+"-"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")+"-"+os.environ.get("SHORT_SHA", "latest"), "source:"+sha256, "signature:"+signature_has],
   extra_headers={"description":"This is an example pipeline template."})
 
