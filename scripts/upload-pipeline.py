5a541507eb5d84c0ed2097dc71416280e23c17e0
Dan <daschwanden@google.com>
2024-06-24 08:47:46 +0000

    Updates the pipeline upload logic

--- scripts/upload-pipeline.py
+++ scripts/upload-pipeline.py
@@ -7,6 +7,10 @@ from kfp.registry import RegistryClient
 from sha256sum import sha256sum
 import hashlib
 
+# Calcualte the pipeline function signature
+# It is important that the signature remains constant
+# Any change to the signature will mean major update,
+# hence a new release should be created (in a new directory).
 def hash_signature(signature):
  # Encode the signature as bytes
  signature_bytes = signature.encode('utf-8')
@@ -16,23 +20,57 @@ def hash_signature(signature):
  signature_hash = hash_object.hexdigest()
  return signature_hash
 
+# Initialise the envrionment
 region = os.environ.get("_REGION", "")
 repo_name = os.environ.get("REPO_NAME", "")
 project_id = os.environ.get("PROJECT_ID", "")
 package = os.environ.get("PACKAGE", "")
 signature = os.environ.get("SIGNATURE", "")
-signature_hash = hash_signature(signature)
 host = "https://"+region+"-kfp.pkg.dev/"+project_id+"/"+repo_name
+
+# Create the Vertex Registry client
 client = RegistryClient(host=host)
 
+# Initialise the settings for the pipeline source and compiled pipeline
 source_name = "pipeline.py"
 compiled_name = "pipeline.yaml"
-sha256 = sha256sum(source_name)
 
-templateName, versionName = client.upload_pipeline(
-  file_name=compiled_name,
-  tags=[os.environ.get("BRANCH_NAME").replace("/", "-")+"-"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")+"-"+os.environ.get("SHORT_SHA", "latest"),
-        "branch:"+os.environ.get("BRANCH_NAME").replace("/", "-"),
-        "source:"+sha256,
-        "signature:"+signature_hash],
-  extra_headers={"description":"This is an example pipeline template."})
+source_sha256 = "source:"+sha256sum(source_name)
+signature_sha256 = "signature:"+hash_signature(signature)
+
+versions = client.list_versions(package)
+firstVersion = false
+
+if len(versions) > 0:
+  # In case we have existing versions of the pipeline make sure the signature has not changed
+  signature_tag = client.get_tag(package, signature_sha256)
+  if not signature_tag:
+    sys.exit("Error message: The pipeline signature has changed! This would break all existing clients.")
+else:
+ firstVersion = true
+
+if firstVersion:
+  # Upload the first version of the pipeline
+  print("Uploading the first version of the pipeline: "+package)
+  packageName, version = client.upload_pipeline(
+    file_name=compiled_name,
+    tags=[os.environ.get("BRANCH_NAME").replace("/", "-")+"-"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")+"-"+os.environ.get("SHORT_SHA", "latest"),
+          "branch:"+os.environ.get("BRANCH_NAME").replace("/", "-"),
+          source_sha256,
+          signature_sha256],
+    extra_headers={"description":"This is an example pipeline template."})
+else:
+  source_tag = client.get_tag(package, source_sha256)
+  if not source_tag:
+    # Upload the updated version of the pipeline
+    print("Uploading a new version of the pipeline: "+package)
+    packageName, version = client.upload_pipeline(
+      file_name=compiled_name,
+      tags=[os.environ.get("BRANCH_NAME").replace("/", "-")+"-"+datetime.datetime.now(datetime.UTC).strftime("%Y%m%dT%H%M%SZ")+"-"+os.environ.get("SHORT_SHA", "latest"),
+            source_sha256],
+      extra_headers={"description":"This is an example pipeline template."})
+    # Move the tags to the new version
+    tag = client.update_tag(package, version, "branch:"+os.environ.get("BRANCH_NAME").replace("/", "-"))
+    tag = client.update_tag(package, version, signature_256)
+  else:
+    print("Nothing to do, we already have a version of the pipeline: "+package)
