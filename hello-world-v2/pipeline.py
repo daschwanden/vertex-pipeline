cdc411049e37398d0472f0db8709b71268025c6a
Dan <daschwanden@google.com>
2024-06-26 17:42:12 +0000

    changes v2

--- hello-world-v2/pipeline.py
+++ hello-world-v2/pipeline.py
@@ -15,7 +15,7 @@ def hello_world(text: str) -> str:
 def pipeline_hello_world(text: str = 'hi there, big world'):
     """Pipeline that passes small pipeline parameter string to consumer op."""
 
-    text = "hello world out there 1234567890."
+    text = "hello world out there 12345678901."
     consume_task = hello_world(
         text=text)  # Passing pipeline parameter as argument to consumer op
 
